60fc6089e7a5719a9c5b0337d46a1c336a8e807b
marclopezsoler <marcls.arbucies@gmail.com>
2023-09-29 03:53:21 +0200

    case convert

--- src/components/CharsBtn.js
+++ src/components/CharsBtn.js
@@ -1,5 +1,5 @@
 export default function CharsBtn({functionName, name}){
    return(
-      <button onClick={{functionName}} className={`transition duration-200 md:hover:scale-105 bg-purple text-white font-bold text-sm px-4 py-2 rounded-full text-white`}>{name}</button>
+      <button onClick={() => functionName()} className={`transition duration-200 md:hover:scale-105 bg-purple text-white font-bold text-sm px-4 py-2 rounded-full text-white`}>{name}</button>
    )
 }
