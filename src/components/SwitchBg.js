697f9396434f9529bc0c10da482fd4c6eef9293c
marclopezsoler <marcls.arbucies@gmail.com>
2023-09-30 13:01:27 +0200

    fixed daylight btn positioning

--- src/components/SwitchBg.js
+++ src/components/SwitchBg.js
@@ -8,7 +8,7 @@ export default function SwitchBg({state, changeBg}) {
   return (
     <button
       onClick={changeBg}
-      className={`${switchBg} sticky z-30 absolute drop-shadow-2xl p-2 w-10 h-10 rounded-full bottom-4 transition duration-500 sm:hover:scale-110`}
+      className={`${switchBg} z-30 fixed drop-shadow-2xl p-2 w-10 h-10 rounded-full bottom-4 transition duration-500 sm:hover:scale-110`}
     >
       <img className="transition duration-500" src={icon} alt="Toggle Background" />
     </button>
